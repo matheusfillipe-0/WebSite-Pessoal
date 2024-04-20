0ebe756e7c942823cb8e1719f5ab403a78136700
matheusfillipe-0 <135716569+matheusfillipe-0@users.noreply.github.com>
2024-04-20 18:34:16 -0300

    Add files via upload

---
+++ PROJETO Av2 (concluído)/script.js
@@ -0,0 +1,8 @@
+function nav_bar (){
+    var navMenu = document.getElementById("navMenu");
+    if (navMenu.style.display === "none") {
+        navMenu.style.display = "block";
+    } else {
+        navMenu.style.display = "none";
+    }
+}
